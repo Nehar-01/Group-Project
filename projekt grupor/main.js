beaa07547951c17188c23aa05c1b111459c95680
reisbreznica1 <reis.breznica1@gmail.com>
2026-06-28 21:58:31 +0200

    commit

--- projekt grupor/main.js
+++ projekt grupor/main.js
@@ -8,7 +8,6 @@ function rërë(){
 
 function shtëpia(){
     var answer = confirm("Dëshironi të mësoni më shumë për shtëpinë në pemë?");
-
     if(answer){
         alert("Shtëpia në pemë është tek livadhi prapa shankut, aty fëmijët mund shikojnë pamjen e natyrës dhe te argëtohen në luhatëse.");
     }
@@ -16,15 +15,13 @@ function shtëpia(){
 
 function këndi(){
     var answer = confirm("Dëshironi të mësoni më shumë për këndin e aventurave?");
-
     if(answer){
-        alert("Këndi i aventurave është vendi ku argëtohen fëmijët me lojrat që i jepin kujtime të pa harrushme.");
+        alert("Këndi i aventurave është vendi ku argëtohen fëmijët med lojrat që i jepin kujtime të pa harrushme.");
     }
 }
 
 function hiking(){
     var answer = confirm("Dëshironi të mësoni më shumë për hikingun në natyrë?");
-
     if(answer){
         alert("Hikingu fillon tek livadhi prapa shankut dhe zhvillohet tek pylli afër fermës i shoqëruar me qenët e fermës dhe zgjatë 45 minuta deri në 1 orë.");
     }
@@ -32,7 +29,6 @@ function hiking(){
 
 function pony(){
     var answer = confirm("Dëshironi të mësoni më shumë për kalërimin me poni?");
-
     if(answer){
         alert("Kalërimi me poni është një aktivitet argëtues për fëmijë, ku fëmijëve u ipet mundësia për të kalëruar me poni me mbikqyerjen e stafit, ky aktivitet është tek livadhi prapa shanku.");
     }
@@ -42,41 +38,45 @@ var images = document.querySelectorAll(".imganimal");
 var overlay2 = document.getElementById("overlay2");
 var overlayImg = document.getElementById("overlayImg");
 
-for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", function () {
-        overlay2.style.display = "flex";
-        overlayImg.src = this.src;
+if (overlay2 && overlayImg) {
+    for (var i = 0; i < images.length; i++) {
+        images[i].addEventListener("click", function () {
+            overlay2.style.display = "flex";
+            overlayImg.src = this.src;
+            
+            var albumi = document.querySelector(".albumi");
+            if (albumi) albumi.style.filter = "blur(3px)";
+        });
+    }
 
-        document.querySelector(".albumi").style.filter = "blur(3px)";
+    overlay2.addEventListener("click", function () {
+        overlay2.style.display = "none";
+        var albumi = document.querySelector(".albumi");
+        if (albumi) albumi.style.filter = "none";
     });
 }
 
-overlay2.addEventListener("click", function () {
-    overlay2.style.display = "none";
-    document.querySelector(".albumi").style.filter = "none";
-});
-
 function showText() {
-    document.getElementById("text").style.display = "block";
+    var textEl = document.getElementById("text");
+    if (textEl) textEl.style.display = "block";
 }
 
 function hideText() {
-    document.getElementById("text").style.display = "none";
+    var textEl = document.getElementById("text");
+    if (textEl) textEl.style.display = "none";
 }
 
 var count = 0;
-
 function hello() {
     var title = document.getElementById("title");
+    if (!title) return;
 
     if (title.innerHTML == "LAZDROHU N'FERMË") {
         title.innerHTML = "Mirë se vini!";
     } else {
         title.innerHTML = "LAZDROHU N'FERMË";
     }
-
     count++;
-
     console.log("Klikime");
 }
 
@@ -108,7 +108,7 @@ function swap2(img) {
     else if (img.src.includes("pauna.jpg")) {
         img.src = "img/kafshet/pauna 2.jpg";
     }
-    else if (img.src.includes("pauna%202.jpg") || img.src.includes("pauna 2.jpg")) {
+    else if (img.src.includes("pauna 2.jpg") || img.src.includes("pauna 2.jpg")) {
         img.src = "img/kafshet/fazan.jpg";
     }
     else {
@@ -125,3 +125,19 @@ function swap3(img) {
     }
 }
 
+function updateFarmStatus() {
+    var statusBadge = document.getElementById("status-badge");
+    if (!statusBadge) return; 
+
+    var currentHour = new Date().getHours();
+
+    if (currentHour >= 12 && currentHour < 20) {
+        statusBadge.innerHTML = "● Hapur tani (12:00 - 20:00)";
+        statusBadge.style.color = "#4CAF50"; 
+    } else {
+        statusBadge.innerHTML = "○ Mbyllur tani (Hapet në 12:00)";
+        statusBadge.style.color = "#f44336"; 
+    }
+}
+
+updateFarmStatus();
